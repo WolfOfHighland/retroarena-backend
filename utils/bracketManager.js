982c6f792f9385b11f4c954984621be8fcc29d93
WolfOfHighland <eaj1074@gmail.com>
2025-11-11 18:00:32 -0500

    Persist bracket state and reward champion with RRP

--- utils/bracketManager.js
+++ utils/bracketManager.js
@@ -1,3 +1,5 @@
+const User = require('../models/User');
+
 function generateBracket(players, matchSize = 2) {
   const bracket = [];
   for (let i = 0; i < players.length; i += matchSize) {
@@ -56,26 +58,29 @@ class BracketManager {
   constructor(io, tournament) {
     this.io = io;
     this.tournament = tournament;
-    this.round = 1;
-    this.winnersByRound = {}; // e.g. { r1: ['Wolf', 'Player2'], r2: [...] }
+    this.winnersByRound = tournament.winnersByRound || {};
+    this.round = Object.keys(this.winnersByRound).length || 1;
   }
 
-  recordResult(matchId, winnerId) {
+  async recordResult(matchId, winnerId) {
     const roundKey = `r${this.round}`;
     if (!this.winnersByRound[roundKey]) {
       this.winnersByRound[roundKey] = [];
     }
 
     this.winnersByRound[roundKey].push(winnerId);
+    this.tournament.winnersByRound = this.winnersByRound;
+    await this.tournament.save();
+
     console.log(`🏆 Winner recorded for ${matchId}: ${winnerId}`);
 
     const expectedWinners = Math.ceil(this.tournament.maxPlayers / Math.pow(2, this.round));
     if (this.winnersByRound[roundKey].length === expectedWinners) {
-      this.advanceRound();
+      await this.advanceRound();
     }
   }
 
-  advanceRound() {
+  async advanceRound() {
     const prevRoundKey = `r${this.round}`;
     const winners = this.winnersByRound[prevRoundKey];
     const nextBracket = advanceWinners(winners);
@@ -83,6 +88,8 @@ class BracketManager {
     this.round += 1;
     const nextRoundKey = `r${this.round}`;
     this.winnersByRound[nextRoundKey] = [];
+    this.tournament.winnersByRound = this.winnersByRound;
+    await this.tournament.save();
 
     if (nextBracket.length === 1 && nextBracket[0].length === 1) {
       const champion = nextBracket[0][0];
@@ -91,6 +98,21 @@ class BracketManager {
         tournamentId: this.tournament.id,
         champion,
       });
+
+      // 🎁 Reward champion with RRP
+      try {
+        const user = await User.findOne({ username: champion });
+        if (user) {
+          user.rrpBalance += 100;
+          await user.save();
+          console.log(`🎉 ${champion} rewarded with 100 RRP`);
+        } else {
+          console.warn("⚠️ Champion not found in User collection:", champion);
+        }
+      } catch (err) {
+        console.error("❌ Error rewarding champion:", err);
+      }
+
       return;
     }
 
